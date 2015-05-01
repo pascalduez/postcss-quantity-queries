dcfe3f2d283d785ef9c2db86464365c28ac6d712
Pascal Duez <public@pascalduez.me>
2015-05-01 13:18:23 +0200

    Update linting

--- test/develop.js
+++ test/develop.js
@@ -9,13 +9,13 @@ function read(name) {
   return fs.readFileSync(path.join(__dirname, 'fixture', name), 'utf8');
 }
 
-['at-least', 'at-most', 'between', 'exactly'].forEach(function (test) {
+[ 'at-least', 'at-most', 'between', 'exactly' ].forEach(function (test) {
   var input = read('atule/' + test + '/input.css');
   var css = postcss(plugin()).process(input).css;
   console.log(css);
 });
 
-['at-least', 'at-most', 'between', 'exactly'].forEach(function (test) {
+[ 'at-least', 'at-most', 'between', 'exactly' ].forEach(function (test) {
   var input = read('pseudo/' + test + '/input.css');
   var css = postcss(plugin()).process(input).css;
   console.log(css);
